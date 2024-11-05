458d436ae974118f6cbd9f92ca3f4fd6855d87fa
Alberto Leon Luengo <a.leon.2020@alumnos.urjc.es>
2024-11-05 09:47:59 +0100

    UPLOADING FILES ...

--- Códigos Libro/ex05/ex05.py
+++ Códigos Libro/ex05/ex05.py
@@ -4,6 +4,14 @@
 import sys
 # * from python_environment_check import check_packages
 from python_environment_check import check_packages
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+import matplotlib.pyplot as plt
+from matplotlib.colors import ListedColormap
+from sklearn.linear_model import LogisticRegression
+from sklearn.decomposition import PCA
 
 # # Machine Learning with PyTorch and Scikit-Learn  
 # # -- Code Examples
@@ -58,11 +66,17 @@ d = {
 check_packages(d)
 
 
-# # Example 5 - ??? Dataset
+# # Example 4 - Wine Dataset
 
 # ### Overview
 
-# ...
+# - [Importing libraries for data analysis and scaling](#importing-libraries-for-data-analysis-and-scaling)
+# - [Extracting the principal components step by step](#extracting-the-principal-components-step-by-step)
+# - [Total and explained variance](#total-and-explained-variance)
+# - [Feature transformation](#feature-transformation)
+# - [Principal component analysis in scikit-learn](#principal-component-analysis-in-scikit-learn)
+# - [Assessing feature contributions](#assessing-feature-contributions)
+# - [Summary](#summary)
 
 
 
@@ -80,9 +94,251 @@ check_packages(d)
 
 
 
-# ## ...
+# ## Importing libraries for data analysis and scaling
 
-# ...
+
+
+
+
+# ## Extracting the principal components step by step
+
+
+
+# Download the wine dataset from the UCI Machine Learning Repository
+df_wine = pd.read_csv('dataset1.csv')
+
+
+
+
+# Show the first five rows of the dataset
+df_wine.head()
+
+
+
+
+# Show the shape of the dataset
+df_wine.shape
+
+
+
+
+# Splitting the dataset into features and target variable
+X, y = df_wine.iloc[:, 0:-1].values, df_wine.iloc[:, -1].values # Target variable is the first column
+
+
+
+
+# Splitting the dataset into training and test sets
+# stratify=y makes sure that the training and test datasets have the same class proportions
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, stratify=y, random_state=0) # 30% test data
+
+
+
+
+# Standardize the features
+sc = StandardScaler()
+X_train_std = sc.fit_transform(X_train)
+X_test_std = sc.transform(X_test)
+
+
+
+
+# Calculate the covariance matrix
+cov_mat = np.cov(X_train_std.T)
+
+
+
+
+# Calculate the eigenvalues and eigenvectors of the covariance matrix
+eigen_vals, eigen_vecs = np.linalg.eig(cov_mat)
+
+
+
+
+print('\nEigenvalues \n', eigen_vals)
+
+
+
+
+print('\nEigenvectors \n', eigen_vecs)
+
+
+# ## Total and explained variance
+
+
+
+# Calculate the explained variance ratio
+tot = sum(eigen_vals)
+var_exp = [(i / tot) for i in sorted(eigen_vals, reverse=True)]
+cum_var_exp = np.cumsum(var_exp)
+
+plt.bar(range(1,9), var_exp, align='center', label='Individual explained variance')
+plt.step(range(1,9), cum_var_exp, where='mid', label='Cumulative explained variance')
+plt.ylabel('Explained variance ratio')
+plt.xlabel('Principal component index')
+plt.legend(loc='best')
+plt.tight_layout()
+plt.show()
+
+
+# ## Feature transformation
+
+
+
+# Make a list of (eigenvalue, eigenvector) tuples
+eigen_pairs = [(np.abs(eigen_vals[i]), eigen_vecs[:, i]) for i in range(len(eigen_vals))]
+
+# Sort the (eigenvalue, eigenvector) tuples from high to low
+eigen_pairs.sort(key=lambda k: k[0], reverse=True)
+
+
+
+
+# Construct the projection matrix 
+# In this case, we will reduce the high-dimensional feature space to a 2-dimensional feature subspace
+w = np.hstack((eigen_pairs[0][1][:, np.newaxis], eigen_pairs[1][1][:, np.newaxis]))
+print('Matrix W:\n', w)
+
+
+
+
+# Transform the first element of the training dataset using the projection matrix
+X_train_std[0].dot(w)
+
+
+
+
+# Transform the entire training dataset using the projection matrix
+X_train_pca = X_train_std.dot(w)
+
+
+
+
+# Visualize the transformed  training dataset in a two-dimensional scatterplot
+colors = ['r', 'b', 'g']
+markers = ['o', 's', '^']
+for l, c, m in zip(np.unique(y_train), colors, markers):
+    plt.scatter(X_train_pca[y_train==l, 0],
+                X_train_pca[y_train==l, 1],
+                c=c, label=f'Class {l}', marker=m)
+plt.xlabel('PC 1')
+plt.ylabel('PC 2')
+plt.legend(loc='lower left')
+plt.tight_layout()
+plt.show()
+
+
+# ## Principal component analysis in scikit-learn
+
+
+
+# Auxiliar function to plot the decision regions
+def plot_decision_regions(X, y, classifier, test_idx=None, resolution=0.02):
+    # setup marker generator and color map
+    markers = ('o', 's', '^', 'v', '<')
+    colors = ('red', 'blue', 'lightgreen', 'gray', 'cyan')
+    cmap = ListedColormap(colors[:len(np.unique(y))])
+    # plot the decision surface
+    x1_min, x1_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+    x2_min, x2_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+    xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution),
+    np.arange(x2_min, x2_max, resolution))
+    lab = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
+    lab = lab.reshape(xx1.shape)
+    plt.contourf(xx1, xx2, lab, alpha=0.3, cmap=cmap)
+    plt.xlim(xx1.min(), xx1.max())
+    plt.ylim(xx2.min(), xx2.max())
+    # plot class examples
+    for idx, cl in enumerate(np.unique(y)):
+        plt.scatter(x=X[y == cl, 0],
+        y=X[y == cl, 1],
+        alpha=0.8,
+        c=colors[idx],
+        marker=markers[idx],
+        label=f'Class {cl}',
+        edgecolor='black')
+
+
+
+
+# scikit-learn
+# Initializing the PCA transformer
+pca = PCA(n_components=2)
+
+# Initializing the logistic regression estimator:
+lr = LogisticRegression(multi_class='ovr', random_state=1, solver='lbfgs') # ovr = One-vs-Rest, lbfgs = Limited-memory Broyden-Fletcher-Goldfarb-Shanno
+
+# Dimensionality reduction
+X_train_pca = pca.fit_transform(X_train_std)
+X_test_pca = pca.transform(X_test_std)
+
+# Fitting the logistic regression model on the reduced dataset:
+lr.fit(X_train_pca, y_train)
+
+# Plotting the decision regions in the reduced space for the training dataset
+plot_decision_regions(X_train_pca, y_train, classifier=lr)
+plt.xlabel('PC 1')
+plt.ylabel('PC 2')
+plt.legend(loc='lower left')
+plt.tight_layout()
+plt.show()
+
+
+
+
+# scikit-learn
+# Plotting the decision regions in the reduced space for the test dataset
+plot_decision_regions(X_test_pca, y_test, classifier=lr)
+plt.xlabel('PC 1')
+plt.ylabel('PC 2')
+plt.legend(loc='lower left')
+plt.tight_layout()
+plt.show()
+
+
+
+
+# scikit-learn
+# Explained variance ratios
+pca = PCA(n_components=None)
+X_train_pca = pca.fit_transform(X_train_std)
+print(pca.explained_variance_ratio_)
+
+
+# ## Assessing feature contributions
+
+
+
+# Calculate the loadings
+loadings = eigen_vecs * np.sqrt(eigen_vals)
+
+# Plot the loadings for the first principal component
+fig, ax = plt.subplots()
+ax.bar(range(8), loadings[:, 0], align='center')
+ax.set_ylabel('Loadings for PC 1')
+ax.set_xticks(range(8))
+ax.set_xticklabels(df_wine.columns[0:-1], rotation=90)
+plt.ylim([-1, 1])
+plt.tight_layout()
+plt.show()
+
+
+
+
+# scikit-learn
+# Calculate the loadings
+sklearn_loadings = pca.components_.T * np.sqrt(pca.explained_variance_)
+
+# Plot the loadings for the first principal component
+fig, ax = plt.subplots()
+ax.bar(range(8), sklearn_loadings[:, 0], align='center')
+ax.set_ylabel('Loadings for PC 1')
+ax.set_xticks(range(8))
+ax.set_xticklabels(df_wine.columns[0:-1], rotation=90)
+plt.ylim([-1, 1])
+plt.tight_layout()
+plt.show()
+
 
 # # Summary
 
@@ -97,7 +353,7 @@ check_packages(d)
 # * !
 # This symbol is used in environments such as Jupyter Notebooks to execute system commands 
 # operational directly from the notebook. In this case, the command is an execution of a 
-# python script.
+# Python Script.
 # * python ../.convert_notebook_to_script.py
 # This command runs a Python script called convert_notebook_to_script.py. This file 
 # is located in the previous directory (../ indicates that it is one level up in the system 
--- Códigos Libro/formulario_ep1/formulario_ep1.py
+++ Códigos Libro/formulario_ep1/formulario_ep1.py
@@ -804,13 +804,13 @@ regr_quadratic = regr.fit(X_train_quadratic, y_train)
 print("Quadratic Model Coefficients:", regr_quadratic.coef_)
 print("Quadratic Model Intercept:", regr_quadratic.intercept_)
 
-# new_data_quadratic = np.array([[7, 1000, 1500]])
+new_data_quadratic = np.array([[2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048]])
 
-# transformed_new_data_quadratic = quadratic.transform(new_data_quadratic)
-# print("Quadratic Trasformed Data:", transformed_new_data_quadratic[0])
+transformed_new_data_quadratic = quadratic.transform(new_data_quadratic)
+print("Quadratic Trasformed Data:", transformed_new_data_quadratic[0])
 
-# predicted_price = regr_quadratic.predict(transformed_new_data_quadratic)
-# print("Predicted SalePrice:", predicted_price)
+predicted_price = regr_quadratic.predict(transformed_new_data_quadratic)
+print("Predicted Target:", predicted_price)
 
 
 
@@ -831,7 +831,7 @@ print("Quadratic Model Intercept:", regr_quadratic.intercept_)
 #     2250000])       # X3 ^ 2
 
 # manual_prediction = np.dot(coefficients, transformed_new_data) + intercept
-# print("Manually Calculated SalePrice:", manual_prediction)
+# print("Manually Calculated Target:", manual_prediction)
 
 
 # Y por último, se calcula el Error Absoluto Medio (MAE), el Error Cuadrático Medio (MSE) y el Coeficiente de Determinación (R²) para poder evaluar la precisión del modelo cuadrático.
@@ -889,13 +889,13 @@ regr_cubic = regr.fit(X_train_cubic, y_train)
 print("Cubic Model Coefficients:", regr_cubic.coef_)
 print("Cubic Model Intercept:", regr_cubic.intercept_)
 
-# new_data_cubic = np.array([[7, 1000, 1500]])
+new_data_cubic = np.array([[2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048]])
 
-# transformed_new_data_cubic = cubic.transform(new_data_cubic)
-# print("Cubic Transformed Data:", transformed_new_data_cubic[0])
+transformed_new_data_cubic = cubic.transform(new_data_cubic)
+print("Cubic Transformed Data:", transformed_new_data_cubic[0])
 
-# predicted_price_cubic = regr_cubic.predict(transformed_new_data_cubic)
-# print("Predicted SalePrice:", predicted_price_cubic)
+predicted_price_cubic = regr_cubic.predict(transformed_new_data_cubic)
+print("Predicted Target:", predicted_price_cubic)
 
 
 
@@ -926,7 +926,7 @@ print("Cubic Model Intercept:", regr_cubic.intercept_)
 #     3375000000])    # X3 ^ 3
 
 # manual_prediction_cubic = np.dot(coefficients_cubic, transformed_new_data_cubic) + intercept_cubic
-# print("Manually Calculated SalePrice:", manual_prediction_cubic)
+# print("Manually Calculated Target:", manual_prediction_cubic)
 
 
 
